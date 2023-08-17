08abd0003656a912ec586dad8b8e1e29c029cc41
Andrew Krippner <andrewkrip@gmail.com>
2023-08-17 16:53:33 -0700

    add bulk api

--- src/enrichContact.ts
+++ src/enrichContact.ts
@@ -1,11 +1,13 @@
 import axios from 'axios'
 
-export const enrichContact = async (contact: {
+interface Contact {
     email?: string
     firstName?: string
     lastName?: string
     companyName?: string
-}) => {
+}
+
+export const enrichContact = async (contact: Contact) => {
     const url = 'https://api.apollo.io/v1/people/match'
     const headers = {
         'Cache-Control': 'no-cache',
@@ -21,4 +23,24 @@ export const enrichContact = async (contact: {
     }
     const res = await axios.post(url, data, { headers })
     return res.data
+}
+
+export const enrichContactsBulk = async (contacts: Contact[]) => {
+    const url = 'https://api.apollo.io/v1/people/bulk_match'
+    const headers = {
+        'Cache-Control': 'no-cache',
+        'Content-Type': 'application/json',
+    }
+    const data = {
+        api_key: process.env.APOLLO_API_KEY,
+        details: contacts.map((contact) => ({
+            email: contact.email,
+            first_name: contact.firstName,
+            last_name: contact.lastName,
+            organization_name: contact.companyName,
+        })),
+        reveal_personal_emails: true,
+    }
+    const res = await axios.post(url, data, { headers })
+    return res.data.matches
 }
